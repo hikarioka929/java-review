2a2b026e516907d49b7d1df9dfac156c78df0ad7
piyoppa <piyoppa@gmail.com>
2021-08-26 01:07:38 +0900

    [add] 演習1

---
+++ src/javaReview1/Ex01.java
@@ -0,0 +1,12 @@
+package javaReview1;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+		int totalNum = 0;
+		for( int i = 1; i <= 100; i++ ) {
+			totalNum += i;
+		}
+		System.out.println(totalNum);
+	}
+}
